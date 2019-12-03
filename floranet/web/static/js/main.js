18eba1217cb0f87758a51aa100cf7dad9a6c2ad5
zhongkai.cheng <zhongkai.cheng>
2019-12-03 04:10:00 +0000

    WEB UI : show bigint by Hex string

--- floranet/web/static/js/main.js
+++ floranet/web/static/js/main.js
@@ -17,26 +17,29 @@ getProp= function(name){
     $.ajax( {
       type: 'get',
       url: '/api/v1.0/'+name,
-      dataType: 'json',
+      dataType: 'text',
       beforeSend: function( xhr, settings ) { xhr.setRequestHeader( 'Authorization', 'IMxHj@wfNkym@*+V85Rs^G<QXMD~p[eaX3S=_D8f7{z0q{GN' ); }
     } )
     .done( ( data ) => {
+      jstr=data.replace(/ (\d+)/g, (m,i)=>{return i.length<8?i:'"'+i+'"'})
+      data = JSON.parse(jstr)
       $('#mainDiv').text("");
       var str='';
       if (data[0] === undefined) {
     	   Object.entries(data).forEach(i=> { 
-                 console.log(i[0], i[1]);
-    		 str+=i[0]+': '+htmlEscape(String(i[1]))+'<br/>'
-             });
-      
+           console.log(i[0], i[1]);
+           if(typeof(i[1])==='string' && i[1].replace(/\d/g,'').length==0)i[1]=BigInt(i[1]).toString(16).toUpperCase()
+            str+=i[0]+': '+htmlEscape(String(i[1]))+'<br/>'
+         });
       }else{
         Object.keys(data).forEach(id=>{
-    	  var app=data[id];
-    	   Object.entries(app).forEach(i=> { 
-                 console.log(i[0], i[1]);
-    		 str+=i[0]+': '+htmlEscape(String(i[1]))+'<br/>'
-             });
-	  str+='------------------------<br/>'	
+          var app=data[id];
+          Object.entries(app).forEach(i=> { 
+            console.log(i[0], i[1]);
+            if(typeof(i[1])==='string' && i[1].replace(/\d/g,'').length==0)i[1]=BigInt(i[1]).toString(16).toUpperCase()
+            str+=i[0]+': '+htmlEscape(String(i[1]))+'<br/>'
+          });
+	        str+='------------------------<br/>'	
         });
       }	      
       $('#mainDiv').html(str);
